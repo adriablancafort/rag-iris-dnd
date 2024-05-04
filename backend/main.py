035c738ab04ccfb08d0df58cbfac81627eb76004
Pol de los Santos Subirats <132887066+PDelos@users.noreply.github.com>
2024-05-04 11:17:28 +0200

    starting up api, finally aboot time
    (I'm canadian)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,36 @@
 from fastapi import FastAPI
 from openai import OpenAI
 
+
+
+
+from langchain_openai import OpenAIEmbeddings
+
+def get_embedding_function():
+    embeddings_model = OpenAIEmbeddings(api_key="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")
+    return embeddings_model
+
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+
+def load_documents(pdf_folder_path: str):
+    document_loader = PyPDFDirectoryLoader(pdf_folder_path)
+    return document_loader.load()
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+
+def split_document(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+
+
 app = FastAPI()
 client = OpenAI()
 
