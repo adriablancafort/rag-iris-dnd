d490be9523f251649c54b901f07cfcb823180931
Eloi Pages <eloipages@gmail.com>
2024-05-04 20:49:31 +0200

    canvis eloi tonto

--- backend/pdf_scrapper.py
+++ backend/pdf_scrapper.py
@@ -1,28 +1,19 @@
 import requests
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader, PyPDFLoader
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 
-
-# Function to load documents from a PDF folder
 def load_documents(pdf_folder_path: str):
     document_loader = PyPDFDirectoryLoader(pdf_folder_path)
     return document_loader.load()
-
-def load_document(pdf_folder_path: str):
-    document_loader = PyPDFLoader(pdf_folder_path)
-    return document_loader.load()
-
+# Function to load PDF
 
 def text_from_pdfURL(URL: str):
-    try:
-        response = requests.get(URL)
-        pdf_path = "data/Downloaded/downloaded_data.pdf"
-        if response.status_code != 200:
-            return "error" + f"bad status code: {response.status_code}"
-        pdf = open(pdf_path, "wb")
-        pdf.write(response.content)
-        pdf.close()
-        documents = load_document(pdf_path).page_content
-        return documents
-    except:
-        return "ERROR"
+    response = requests.get(URL)
+    pdf_path = "data/Downloaded"
+    pdf = open(pdf_path + "/downloaded_data.pdf", "wb")
+    pdf.write(response.content)
+    pdf.close()
+    document = load_documents(pdf_path)
+    return document
 
+
+text_from_pdfURL("https://orkerhulen.dk/onewebmedia/DnD%205e%20Players%20Handbook%20%28BnW%20OCR%29.pdf")
