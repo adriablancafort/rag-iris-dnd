e231dc399bc44ab632383d9b7a5838cecdd12c06
Adrià Blancafort <76774853+adriablancafort@users.noreply.github.com>
2024-05-04 08:26:56 +0200

    FastAPI

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from openai import OpenAI
+
+app = FastAPI()
+
+client = OpenAI(
+    api_key="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"
+)
+
+
+@app.get("/")
+async def root():
+    return {"response": "OK"}
+
+
+@app.get("/ask/{prompt}")
+def ask(prompt: str):
+    return {"response": get_response(prompt)}
+
+
+def get_response(prompt: str):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return completion.choices[0].message.content
